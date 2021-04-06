f45e8170fb7df9a1436495ae492306f890a8e907
ProScripter <60108520+AdoptMeHub@users.noreply.github.com>
2021-04-06 18:55:52 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -23,6 +23,8 @@ elseif game.PlaceId ==5293755937 then
     T:Load("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/Speed%20Simulator")
 elseif game.PlaceId ==189707 then
     T:Load("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/Natural%20Disaster%20Survival")
+elseif game.PlaceId ==23578803 then
+    T:Load("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/Hotel%20Elephant")
 else
     game:GetService("Players").LocalPlayer:Kick("Game Not Supported")
 end 
