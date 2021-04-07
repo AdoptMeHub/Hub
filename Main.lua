a3e603e61e874393a3f1a948fa85486b43ecc9f3
ProScripter <60108520+AdoptMeHub@users.noreply.github.com>
2021-04-07 16:36:18 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,14 +1,6 @@
 local T = loadstring(game:HttpGet("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/.lua"))()
-if game.PlaceId == 920587237 then
-    T:Load("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/AdoptMe")
-elseif game.PlaceId == 142823291 then
-    T:Load("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/Murder%20Mystery%202")
-elseif game.PlaceId ==735030788 then
-    T:Load("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/Royale%20High")
-elseif game.PlaceId ==566399244 then
-    T:Load("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/Elemental%20BattleGrounds")   
-elseif game.PlaceId ==370731277 then
-    T:Load("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/MeepCity")
+if game.PlaceId == 370731277 then
+ T:Load("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/MeepCity")
 elseif game.PlaceId ==6447798030 then
     T:Load("https://raw.githubusercontent.com/AdoptMeHub/Hub/main/Funky%20Friday")
 elseif game.PlaceId ==621129760 then
